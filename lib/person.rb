e7323500f671d07cd6334b74390fce223310970c
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-04-08 22:42:50 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+  
+  def name=(whats_name)
+    @name = whats_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(where_work)
+    @job = where_work
+  end
+  
+  def job
+    @job
+  end
+  
+end
