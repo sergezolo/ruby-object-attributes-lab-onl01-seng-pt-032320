c61bacaf17b5c21ad639324ea9516584fff0d0a5
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-04-08 22:39:37 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  
+  def name=(dogs_name)
+    @hey_dog = dogs_name
+  end
+  
+  def name
+    @hey_dog
+  end
+  
+  def breed=(whats_breed)
+    @dogs_breed = whats_breed
+  end
+  
+  def breed
+    @dogs_breed
+  end
+  
+end
