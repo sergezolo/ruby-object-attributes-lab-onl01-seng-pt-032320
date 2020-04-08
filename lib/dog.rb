49c986fdc0818e4902131df0597eeef03ba7e6d6
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-04-08 22:43:31 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def breed=(whats_breed)
+    @breed = whats_breed
+  end
+  
+  def breed
+    @breed
+  end
+  
+end
